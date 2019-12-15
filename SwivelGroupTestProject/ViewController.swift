bbc3f2cf68499a900f3af4b3b2fd52153b5d160c
Gihan <gihan@ceylonsolutions.com>
2019-12-15 09:07:46 +0530

    Initial Commit

---
+++ SwivelGroupTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwivelGroupTestProject
+//
+//  Created by Gihan on 12/15/19.
+//  Copyright © 2019 Gihan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
